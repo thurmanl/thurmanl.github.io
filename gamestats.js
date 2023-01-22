8d9ddb1ae11511bd6837a93aac690e075f219de2
thurmanl <70459537+thurmanl@users.noreply.github.com>
2023-01-21 23:37:59 -0700

    Test on load stuff

--- gamestats.js
+++ gamestats.js
@@ -1,10 +1,15 @@
+
 document.addEventListener('DOMContentLoaded', (event) => {
   alert('DOM fully loaded and parsed');
+});
+
+window.addEventListener('load', (event) => {
+  alert('page fully loaded and parsed');
 });
 
 window.onunload = function() {
     //myfun();
     var path = window.location.pathname;
     var page = path.split("/").pop();
-    console.log("Left page " + page);
+    alert("Left page " + page);
 }
