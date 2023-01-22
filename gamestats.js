ca741d5ed8a8d6b2db93181ebe414da373bb990a
thurmanl <70459537+thurmanl@users.noreply.github.com>
2023-01-21 23:51:51 -0700

    Test on load stuff

--- gamestats.js
+++ gamestats.js
@@ -1,8 +1,9 @@
-//works on first load, but not after
-window.addEventListener('load', (event) => {
-  alert('page fully loaded and parsed');
-});
+// //works on first load, but not after
+// window.addEventListener('load', (event) => {
+//   alert('page fully loaded and parsed');
+// });
 
+//same here
 window.addEventListener('pageshow', (event) => {
   var path = window.location.pathname;
   var page = path.split("/").pop();
@@ -16,7 +17,7 @@ window.onunload = function() {
     alert("Left page " + page);
 }
 
-navigation.addEventListener('navigate', (event) => {
+window.navigation.addEventListener('navigate', (event) => {
   var path = window.location.pathname;
   var page = path.split("/").pop();
   alert("page navigation on" + page);
