6324e3713291c0ba94f8373ea2c2cb146ad56b67
thurmanl <70459537+thurmanl@users.noreply.github.com>
2023-01-21 22:46:34 -0700

    Add I Meta Virus

--- gamestats.js
+++ gamestats.js
@@ -1,3 +1,10 @@
 document.addEventListener('DOMContentLoaded', (event) => {
   alert('DOM fully loaded and parsed');
 });
+
+window.onunload = function() {
+    //myfun();
+    var path = window.location.pathname;
+    var page = path.split("/").pop();
+    console.log("Left page " + page);
+}
