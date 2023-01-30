fb305442f4dac41f43c3a95cc5ce411d37a16bbb
thurmanl <70459537+thurmanl@users.noreply.github.com>
2023-01-29 21:44:42 -0700

    Add Cave Rave Dave

--- gamestats.js
+++ gamestats.js
@@ -1,31 +1,52 @@
-// //works on first load, but not after
-// window.addEventListener('load', (event) => {
-//   alert('page fully loaded and parsed');
-// });
+// testJson();
+// setInterval(function(){ reportPage();}, 1000);
 
-//same here
-window.addEventListener('pageshow', (event) => {
-  var path = window.location.pathname;
-  var page = path.split("/").pop();
-  alert("on page " + page);
-  setInterval(1, reportPage);
-});
-
-// window.onunload = function() {
-//     //myfun();
-//     var path = window.location.pathname;
-//     var page = path.split("/").pop();
-//     alert("Left page " + page);
-// }
-
-// window.navigator.addEventListener('navigate', (event) => {
+// function reportPage() {
 //   var path = window.location.pathname;
 //   var page = path.split("/").pop();
-//   alert("page navigation on" + page);
+//   console.log("on page " + page);
+// }
+
+// function testJson(){
+//   // const fs = require('fs');
+//   // const path = require('path');
+
+//   const testObject = {
+//     hello: 'world',
+//     myArray: [
+//       'entry1',
+//       'entry2',
+//       'entry3',
+//     ],
+//     myNestedObject: {
+//       nestedHello: 'nestedWorld',
+//     },
+//   };
+
+//   const testJsonString = JSON.stringify(testObject, null, 2);
+//   console.log(testJsonString);
+
+//   fetch("theFile.json") 
+//   .then( function(data){return data.json() })
+//   .then(function(response){console.log(response)});
+//   // const filePath = path.join(process.cwd(), 'test.json');
+
+//   // fs.writeFile(filePath, testJsonString, (err) => {
+//   //   if (err) {
+//   //     console.error(err);
+//   //   } else {
+//   //     console.log('File written successfully');
+//   //   }
+//   // });
+// }
+
+// document.addEventListener('visibilitychange', function (event) {
+//   if (document.hidden) {
+//     interacted = false;
+//   } else {
+//     interacted = true;
+//   }
 // });
 
-function reportPage(){
-  var path = window.location.pathname;
-  var page = path.split("/").pop();
-  console.log("on page:" + page);
-}
+// var interacted;
+// onmousemove = (event) => {interacted = true; };
